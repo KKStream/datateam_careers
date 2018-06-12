ba74487c31e39f87a0d74127225ccbb132ed3e4b
ironhead <ironhead.chuang@gmail.com>
2018-06-12 15:37:56 +0800

    - add descriptions for assignment 01

---
+++ assignment_00/experiments.py
@@ -0,0 +1,23 @@
+"""
+Read the competition description and get the raw dataset. Design features for
+the competition and write code to do the preprocessing. Send us your workable
+code and briefly explain why you chose those features.
+
+
+Things you can do:
+    * Build your own features and manipulate the raw dataset.
+    * Read assignment_01 and process the raw dataset as its description.
+    * Do it without pandas.
+    * Do it with multiprocessing.
+    * Do it with pyspark.
+
+Note:
+    * THIS IS NO BIG DATA. Your code must process the entire dataset.
+    * Although this is not a coding style test, please make the code readable,
+      please...
+
+FYR:
+    On a normal machine (Intel(R) Core(TM) i5-4440 CPU @ 3.10GHz), the entire
+    dataset can be transformed to the version for assignment_01 within 5
+    minutes (without optimization).
+"""
